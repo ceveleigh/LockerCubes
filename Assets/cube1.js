e2c31fa03422b99cfd67d1143e49f6e67a7a9732
Carl Eveleigh <carl.eveleigh@gmail.com>
2012-03-06 01:24:00 -0500

    Committing these bitches

---
+++ Assets/cube1.js
@@ -0,0 +1,127 @@
+public var cube1 : GameObject;
+
+private var reset : boolean;
+
+private var one : boolean;
+private var two : boolean;
+private var three : boolean;
+private var four : boolean;
+private var five : boolean;
+private var six : boolean;
+private var oneDimensions : Vector3;
+private var twoDimensions : Vector3;
+private var threeDimensions : Vector3;
+private var fourDimensions : Vector3;
+private var fiveDimensions : Vector3;
+private var sixDimensions : Vector3;
+
+private var rotationDeg : float;
+
+private var startCube : boolean;
+private var startCubeTime : float;
+
+function Start () {
+	//stages in rotation
+	one=true;
+	two=false;
+	three=false;
+	four=false;
+	five=false;
+	six=false;
+	
+	rotationDeg = .1;
+	
+	//set dimensions for cube so it lerps to the ratio of the locker at all angles
+	oneDimensions = Vector3(0.925,1.1,0.925);
+	twoDimensions = Vector3(1.1,0.925,0.925);
+	threeDimensions = Vector3(1.1,0.925,0.925);
+	fourDimensions = Vector3(1.1,.925,1.1);
+	fiveDimensions = Vector3(.925,1.1,1.1);
+	sixDimensions = Vector3(0.925,1.1,0.925);
+	
+	//reset rotation
+	reset = true;
+	startCube = false;
+}
+
+
+function Update () {
+	if(startCubeTime  > 400){
+		startCube = true;
+		startCubeTime = 0;	
+	}
+	startCubeTime+=1;
+	Debug.Log(startCubeTime);
+		//reset position
+		if(reset){
+				cube1.transform.rotation = Quaternion.identity;
+				one = true; //start spinning
+				reset=false;
+				startCube=false;
+		}
+		
+	if(startCube){
+		
+	//spin cycles
+	if(one){
+		cube1.transform.Rotate(Vector3(0,rotationDeg,0) * 20);
+		cube1.transform.localScale = Vector3.Lerp(cube1.transform.localScale,oneDimensions,Time.deltaTime);
+		cube1.transform.position.z = Mathf.Lerp(cube1.transform.position.z, -2.482915, Time.deltaTime);
+		if(cube1.transform.eulerAngles.y >= 90){
+				two=true;
+			one=false;
+		}
+		Debug.Log("step1 - spin left");
+	}
+	if(two){
+		cube1.transform.Rotate(Vector3(0,0,rotationDeg) * 20);
+		cube1.transform.localScale = Vector3.Lerp(cube1.transform.localScale,twoDimensions,Time.deltaTime);
+			cube1.transform.position.z = Mathf.Lerp(cube1.transform.position.z, -6, Time.deltaTime);
+		if(cube1.transform.eulerAngles.z >= 90){
+			three=true;
+			two=false;
+		}
+		Debug.Log("step2 - spin up");
+	}
+	if(three){
+		cube1.transform.Rotate(Vector3(-rotationDeg,0,0) * 20);
+		cube1.transform.localScale = Vector3.Lerp(cube1.transform.localScale,threeDimensions,Time.deltaTime);
+			cube1.transform.position.z = Mathf.Lerp(cube1.transform.position.z, -2.482915, Time.deltaTime);
+		if(cube1.transform.eulerAngles.y <=2){
+			four=true;
+			three=false;
+		}
+		Debug.Log("step3 - spin right");
+	}
+	if(four){
+		cube1.transform.Rotate(Vector3(0,rotationDeg,0) * 20);
+		cube1.transform.localScale = Vector3.Lerp(cube1.transform.localScale,fourDimensions,Time.deltaTime);
+			cube1.transform.position.z = Mathf.Lerp(cube1.transform.position.z, -6, Time.deltaTime);
+		if(cube1.transform.eulerAngles.x <= 270){
+			five=true;
+			four=false;
+		}
+		Debug.Log("step4 - spin down");
+	}
+	if(five){
+		cube1.transform.Rotate(Vector3(0,0,rotationDeg) * -20);
+		cube1.transform.localScale = Vector3.Lerp(cube1.transform.localScale,fiveDimensions,Time.deltaTime);
+		cube1.transform.position.z = Mathf.Lerp(cube1.transform.position.z, -2.482915, Time.deltaTime);
+		if(cube1.transform.eulerAngles.y <= 1){
+			six=true;
+			five=false;
+		}
+		Debug.Log("step5 - spin right");
+	}
+	if(six){
+		cube1.transform.Rotate(Vector3(rotationDeg,0,0) * 10);
+		cube1.transform.localScale = Vector3.Lerp(cube1.transform.localScale,sixDimensions,Time.deltaTime);
+				cube1.transform.position.z = Mathf.Lerp(cube1.transform.position.z, -5.482915, Time.deltaTime);
+		if(cube1.transform.eulerAngles.x >= 358){
+			reset=true; //reset postion
+			six=false;
+		}
+		Debug.Log("step6 - spin up");
+	}
+	}
+}
---
+++ Assets/cube4.js
@@ -0,0 +1,127 @@
+public var cube4 : GameObject;
+
+private var reset : boolean;
+
+private var one : boolean;
+private var two : boolean;
+private var three : boolean;
+private var four : boolean;
+private var five : boolean;
+private var six : boolean;
+private var oneDimensions : Vector3;
+private var twoDimensions : Vector3;
+private var threeDimensions : Vector3;
+private var fourDimensions : Vector3;
+private var fiveDimensions : Vector3;
+private var sixDimensions : Vector3;
+
+private var rotationDeg : float;
+
+private var startCube : boolean;
+private var startCubeTime : float;
+
+function Start () {
+	//stages in rotation
+	one=true;
+	two=false;
+	three=false;
+	four=false;
+	five=false;
+	six=false;
+	
+	rotationDeg = .1;
+	
+	//set dimensions for cube so it lerps to the ratio of the locker at all angles
+	oneDimensions = Vector3(0.925,1.1,0.925);
+	twoDimensions = Vector3(1.1,0.925,0.925);
+	threeDimensions = Vector3(1.1,0.925,0.925);
+	fourDimensions = Vector3(1.1,.925,1.1);
+	fiveDimensions = Vector3(.925,1.1,1.1);
+	sixDimensions = Vector3(0.925,1.1,0.925);
+	
+	//reset rotation
+	reset = true;
+	startCube = false;
+}
+
+
+function Update () {
+	if(startCubeTime  > 320){
+		startCube = true;
+		startCubeTime = 0;	
+	}
+	startCubeTime+=1;
+	Debug.Log(startCubeTime);
+		//reset position
+		if(reset){
+				cube4.transform.rotation = Quaternion.identity;
+				one = true; //start spinning
+				reset=false;
+				startCube=false;
+		}
+		
+	if(startCube){
+		
+	//spin cycles
+	if(one){
+		cube4.transform.Rotate(Vector3(0,rotationDeg,0) * 20);
+		cube4.transform.localScale = Vector3.Lerp(cube4.transform.localScale,oneDimensions,Time.deltaTime);
+		cube4.transform.position.z = Mathf.Lerp(cube4.transform.position.z, -2.482915, Time.deltaTime);
+		if(cube4.transform.eulerAngles.y >= 90){
+				two=true;
+			one=false;
+		}
+		Debug.Log("step1 - spin left");
+	}
+	if(two){
+		cube4.transform.Rotate(Vector3(0,0,rotationDeg) * 20);
+		cube4.transform.localScale = Vector3.Lerp(cube4.transform.localScale,twoDimensions,Time.deltaTime);
+			cube4.transform.position.z = Mathf.Lerp(cube4.transform.position.z, -6, Time.deltaTime);
+		if(cube4.transform.eulerAngles.z >= 90){
+			three=true;
+			two=false;
+		}
+		Debug.Log("step2 - spin up");
+	}
+	if(three){
+		cube4.transform.Rotate(Vector3(-rotationDeg,0,0) * 20);
+		cube4.transform.localScale = Vector3.Lerp(cube4.transform.localScale,threeDimensions,Time.deltaTime);
+			cube4.transform.position.z = Mathf.Lerp(cube4.transform.position.z, -2.482915, Time.deltaTime);
+		if(cube4.transform.eulerAngles.y <=2){
+			four=true;
+			three=false;
+		}
+		Debug.Log("step3 - spin right");
+	}
+	if(four){
+		cube4.transform.Rotate(Vector3(0,rotationDeg,0) * 20);
+		cube4.transform.localScale = Vector3.Lerp(cube4.transform.localScale,fourDimensions,Time.deltaTime);
+			cube4.transform.position.z = Mathf.Lerp(cube4.transform.position.z, -6, Time.deltaTime);
+		if(cube4.transform.eulerAngles.x <= 270){
+			five=true;
+			four=false;
+		}
+		Debug.Log("step4 - spin down");
+	}
+	if(five){
+		cube4.transform.Rotate(Vector3(0,0,rotationDeg) * -20);
+		cube4.transform.localScale = Vector3.Lerp(cube4.transform.localScale,fiveDimensions,Time.deltaTime);
+		cube4.transform.position.z = Mathf.Lerp(cube4.transform.position.z, -2.482915, Time.deltaTime);
+		if(cube4.transform.eulerAngles.y <= 1){
+			six=true;
+			five=false;
+		}
+		Debug.Log("step5 - spin right");
+	}
+	if(six){
+		cube4.transform.Rotate(Vector3(rotationDeg,0,0) * 10);
+		cube4.transform.localScale = Vector3.Lerp(cube4.transform.localScale,sixDimensions,Time.deltaTime);
+				cube4.transform.position.z = Mathf.Lerp(cube4.transform.position.z, -5.482915, Time.deltaTime);
+		if(cube4.transform.eulerAngles.x >= 358){
+			reset=true; //reset postion
+			six=false;
+		}
+		Debug.Log("step6 - spin up");
+	}
+	}
+}
